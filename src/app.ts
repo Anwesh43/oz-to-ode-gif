f885428035a156ba938fed27fc4b4695cb7b957e
Anwesh43 <anweshthecool0@gmail.com>
2021-01-26 03:44:07 +0530

    adding DrawingUtil, ScaleUtil and OzToOd class

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ const delay : number = 20
 const strokeFactor : number = 90 
 const backColor : string = "black"
 const foreColor : string = "white"
+const w : number = 500 
+const h : number = 500 
 
 class State {
 
@@ -37,5 +39,94 @@ class Loop {
             this.animated = false 
             clearInterval(this.interval)
         }
+    }
+}
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+
+    static sinify(scale : number) : number {
+        return Math.sin(scale * Math.PI)
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(
+        context : CanvasRenderingContext2D,
+        x1 : number,
+        y1 : number,
+        x2 : number,
+        y2 : number
+    ) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawDot(
+        context : CanvasRenderingContext2D, 
+        x : number, 
+        y : number, 
+        r : number
+    ) {
+        context.beginPath()
+        context.arc(x, y, r, 0, 2 * Math.PI)
+        context.fill()
+    }
+
+    static drawOText(
+        context : CanvasRenderingContext2D,
+        scale : number
+    ) {
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const text : string = "O"
+        const size : number = Math.min(w, h) / 3
+        context.font = context.font.replace(/\d{2}/, `${size}`)
+        const tw : number = context.measureText(text).width
+        context.fillStyle = foreColor 
+        context.strokeStyle = foreColor  
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.save()
+        context.translate(w / 2 - tw / 2, h / 2 - size / 4)
+        context.fillText(text, 0, 0)
+        for (var j =0 ; j < 2; j++) {
+            context.save()
+            context.scale(1 - 2 * j, 1)
+            DrawingUtil.drawDot(
+                context, 
+                -tw / 3 + sf1 * (tw / 3), 
+                -size * 0.7 + size * 0.7 * sf2 - size * 0.7 * (1 - 2 * j) * sf3,
+                size / 9 
+            )
+            context.restore()
+        }
+        context.restore()
+    }
+}
+
+class OzToOd {
+
+    state : State = new State()
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawOText(context, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
     }
 }
